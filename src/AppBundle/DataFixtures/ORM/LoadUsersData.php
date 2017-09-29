bd7e8808e385cef76a202c4a9798ad379559a6b8
Danny Weeks <danny@dannyweeks.com>
2017-09-29 19:05:08 +0100

    Add some extra users for testing

--- src/AppBundle/DataFixtures/ORM/LoadUsersData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUsersData.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\User;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use FOS\UserBundle\Doctrine\UserManager;
+use FOS\UserBundle\Model\UserInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
@@ -48,8 +49,34 @@ class LoadUsersData implements FixtureInterface, ContainerAwareInterface
         $userManager->updateUser($adminUser);
         $userManager->updateUser($user);
 
+        for($i = 0; $i < 5; $i++) {
+            $userManager->updateUser($this->getRandomUser());
+        }
+
         // You can flush by passing `true` as the second argument to `updateUser()`
         // But I feel like it's more evident if we do it explicitly.
         $objectManager->flush();
+    }
+
+    /**
+     * @return UserInterface
+     */
+    protected function getRandomUser()
+    {
+        $names = ['James','John','Robert','Michael','William','David','Richard','Charles','Joseph','Thomas','Christopher','Daniel'];
+
+        $firstname = $names[rand(0, count($names) - 1)];
+        $surname = $names[rand(0, count($names) - 1)];
+
+        return $this->container
+            ->get('fos_user.user_manager')
+            ->createUser()
+            ->setUsername($firstname . $surname)
+            ->setEmail($firstname . $surname. '@example.com')
+            ->setPlainPassword('user')
+            ->setEnabled(true)
+            ->setRoles(['ROLE_USER'])
+            ->setFirstName($firstname)
+            ->setLastName($surname);
     }
 }
