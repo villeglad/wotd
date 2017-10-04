9874f3d0759f00c054348c3cd54ffdb0bde9dd36
Ville Glad <ville.j.glad@gmail.com>
2017-10-04 10:32:45 +0300

    Add functional test setup

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -2,17 +2,34 @@
 
 namespace Tests\AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use AppBundle\Entity\Word;
+use Tests\FunctionalTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class DefaultControllerTest extends FunctionalTestCase
 {
     public function testSeeAboutPage()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/about');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $crawler = $this->client->request('GET', '/about');
+        $this->assertResponseOk();
         $this->assertContains('About', $crawler->filter('h1')->text());
+    }
+
+    public function testSeeDefaultWordOnIndexPage()
+    {
+        $crawler = $this->client->request('GET', '/');
+        $this->assertResponseOk();
+        $this->assertContains('Missing', $crawler->filter('h1')->text());
+    }
+
+    public function testSeeRandomWordOnIndexPage()
+    {
+        $word = new Word();
+        $word->setWord('Example');
+        $this->entityManager->persist($word);
+        $this->entityManager->flush();
+
+        $crawler = $this->client->request('GET', '/');
+        $this->assertResponseOk();
+        $this->assertContains('Example', $crawler->filter('h1')->text());
     }
 }
---
+++ tests/FunctionalTestCase.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests;
+
+use Doctrine\ORM\Tools\SchemaTool;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class FunctionalTestCase extends WebTestCase
+{
+    protected $client;
+    protected $container;
+    protected $entityManager;
+    protected $schemaTool;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->client = static::createClient();
+        $this->setContainer();
+        $this->entityManager = $this->container->get('doctrine.orm.entity_manager');
+        $this->schemaTool = $this->createSchemaTool();
+    }
+
+    public function setContainer()
+    {
+        if (!isset($this->client)) {
+            $this->client = static::createClient();
+        }
+        if (!isset($this->container)) {
+            $this->container = static::$kernel->getContainer();
+        }
+    }
+
+    private function createSchemaTool()
+    {
+        $schemaTool = new SchemaTool($this->entityManager);
+        $schemaTool->dropDatabase();
+        $schemaTool->createSchema($this->entityManager->getMetadataFactory()->getAllMetadata());
+
+        return $schemaTool;
+    }
+
+    public function assertResponseOk()
+    {
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+}
